c665be2c2cb6a8103e92ae141e4f46e429603155
RUSSELL HARROWER <russell@drn1.com.au>
2021-02-02 15:51:58 +0800

    UPDATE of code
    Working media player using exo-av

--- App.js
+++ App.js
@@ -1,23 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import * as React from 'react';
+import { Text, View, StyleSheet, Button } from 'react-native';
+import { Audio } from 'expo-av';
 
 export default function App() {
+  const [sound, setSound] = React.useState();
+
+  async function playSound() {
+    console.log('Loading Sound');
+    const sound = new Audio.Sound();
+    try {
+      await sound.loadAsync({uri: 'http://stream.radiomedia.com.au:8003/stream'});
+      await sound.playAsync();
+      // Your sound is playing!
+    
+      // Don't forget to unload the sound from memory
+      // when you are done using the Sound object
+     // await sound.unloadAsync();
+    } catch (error) {
+      // An error occurred!
+      print(error)
+    }
+    setSound(sound);
+
+    console.log('Playing Sound');
+    await sound.playAsync(); }
+
+  React.useEffect(() => {
+    return sound
+      ? () => {
+          console.log('Unloading Sound');
+          sound.unloadAsync(); }
+      : undefined;
+  }, [sound]);
 
   return (
-    <View style={styles.container}>
-      <Text>tests</Text>
-      <StatusBar style="auto" />
+    <View>
+      <Button title="Play Sound" onPress={playSound} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
